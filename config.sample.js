f84b4c2ee2e2171ecf146079ba1b3eee5eb6de85
chr0n1x <heilong24@gmail.com>
2014-03-25 12:19:54 -0400

    sample config: updated schema for push events

--- config.sample.js
+++ config.sample.js
@@ -11,7 +11,7 @@ exports.config = {
 			slaveOk: true
 		},
 		database: 'mergeatron',
-		collections: [ 'pulls' ]
+		collections: [ 'pulls', 'pushes' ]
 	},
 	log_level: 'info',
 	plugin_dirs: [ './plugins/' ],
@@ -25,6 +25,9 @@ exports.config = {
 			},
 			user: 'user-to-watch',
 			repos: [ 'repo_name' ],
+			push_repos: {
+			  "repo": [ /^master$/ ]
+			},
 			retry_whitelist: [ 'user', 'user2' ],    // optional whitelist of those able to trigger retries
 			skip_file_listing: false,
 			frequency: 15000,    // only necessary if method is 'polling'
@@ -40,12 +43,22 @@ exports.config = {
 			user: false,
 			pass: false,
 			protocol: 'http',
+			token: 'token_to_remote_trigger_all_jobs',
 			host: 'jenkins.yoururl.com:8080',
+			push_projects: [
+			    REPO_NAME: {
+			        project: 'project_name',
+			        rules: [
+			            // array of RegExps
+			        ],
+			        token: 'special_token_just_for_this_job'
+			    }
+			],
 			projects: [{
 				name: 'project_name',
 				repo: 'repo_name',
-				token: 'token',
-				rules: [ new RegExp(/.php/g) ]
+				token: 'special_token_just_for_this_job',
+				rules: [ /.php/g ]
 			}],
 			frequency: 2000
 		},
